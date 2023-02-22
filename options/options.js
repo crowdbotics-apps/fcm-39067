66b5cae7a9bdbd328dc1c5883833c6d846fff15c
Suhrab developer <suhrab.developer@gmail.com>
2023-02-22 22:32:51 +0430

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "fcm-39067",
   url: "https://fcm-39067.botics.co",
   api: "https://fcm-39067.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
